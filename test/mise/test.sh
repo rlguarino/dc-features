d9822b486a9e352ad88e7d6286e9ba6780c057ef
Ross Guarino <dev@rlg.io>
2025-10-02 01:04:25 +0000

    remote mise doctor from tests

--- test/mise/test.sh
+++ test/mise/test.sh
@@ -40,8 +40,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check "validate mise version" mise version | grep '2025.9.7'
-check "mise doctor" mise doctor
+check "validate mise version" mise version
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
